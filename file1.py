19d9f018351d54113714dd87f16c0cd62450b3cd
Osman Nuri Hömek <osman.homek@uzay.tubitak.gov.tr>
2017-12-21 11:04:27 +0300

    guncellenmis amend

--- file1.py
+++ file1.py
@@ -2,4 +2,9 @@ test icerik 1. satir
 icerige ait 2. satiri da ekledim.
 
 second commit icerisine alacagim
+<<<<<<< Updated upstream
 -- stash ile komutunu calistirdim 
+=======
+
+stash için bir güncelleme yapalım.
+>>>>>>> Stashed changes
